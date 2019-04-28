95e1de26fc2e1c7230f54ce547a807e67453d429
werdwar <drewlw2@illinois.edu>
2019-04-28 14:40:40 -0500

    Added Jsoup html parser API; updated CA class; added scroll view;

--- app/src/main/java/example/com/catrack_o_matic/CourseAssistant.java
+++ app/src/main/java/example/com/catrack_o_matic/CourseAssistant.java
@@ -1,9 +1,10 @@
 package example.com.catrack_o_matic;
 
-public class CourseAssistant {
+public class CourseAssistant implements Comparable {
     private String name;
-
     private String netID;
+    private String imageURL;
+    private int shiftStart;
 
 
     CourseAssistant(String setNetID) {
@@ -21,5 +22,21 @@ public class CourseAssistant {
 
     public String getNetID() {
         return netID;
+    }
+
+    public String getImageURL() { return imageURL; }
+
+    public int compareTo(Object other) {
+        if (!(other instanceof CourseAssistant)) {
+            return -1;
+        }
+        CourseAssistant otherAsCA = (CourseAssistant) other;
+        if (this.shiftStart > otherAsCA.shiftStart) {
+            return 1;
+        }
+        if (this.shiftStart < otherAsCA.shiftStart) {
+            return -1;
+        }
+        return 0;
     }
 }
--- app/src/main/java/example/com/catrack_o_matic/MainActivity.java
+++ app/src/main/java/example/com/catrack_o_matic/MainActivity.java
@@ -2,14 +2,22 @@ package example.com.catrack_o_matic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
-import android.app.Activity;
+//import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Bundle;
+//import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.List;
+//import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,13 +26,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CourseAssistant[] caOnDuty = new CourseAssistant[6];
-        //set this to the current CA's
-        CourseAssistant[] caOnDeck = new CourseAssistant[6];
-        //set this to the next CA's
-        //Always have an array of size 6 so that all slots can be filled to avoid arrayIndexOutOfBounds
-
-
+        //Sets up button with survey link.
         Button redirect = findViewById(R.id.redirectButton);
         redirect.setOnClickListener(new OnClickListener() {
             public void onClick(View arg) {
@@ -33,5 +35,23 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(viewIntent);
             }
         });
+
+        List<CourseAssistant> courseAssistants = new ArrayList<>();
+
+        //Parse CS125 forum html and get photos and names of CAs and TAs
+        try {
+            Document doc = Jsoup.connect("https://cs125.cs.illinois.edu/info/people/#cas").get();
+            Elements assistantElements = doc.body().getElementById("content").getElementsByClass("row flex-x1-nowrap").get(0)
+                    .getElementsByClass("col-12 col-md-9 col-x1-7 py-3").get(0)
+                    .getElementsByClass("mt-3").get(0)
+                    .getElementsByClass("row").get(0)
+                    .getElementsByClass("col-12 col-sm-6 col-lg-4 mt-2");
+
+
+
+        } catch (Exception e) {
+            //set image view to empty/default picture
+        }
+        //for each ca and ta make a new image view and add it to the correct scroll view
     }
 }
